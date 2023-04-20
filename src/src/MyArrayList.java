54c7bf300bbac8a049e6fc0af50647b342ac8027
Zhkslkn <zhkslkn04@gmail.com>
2023-04-20 13:37:04 +0600

    MyArrayList added functions

--- src/src/MyArrayList.java
+++ src/src/MyArrayList.java
@@ -1,4 +1,59 @@
-public class MyArrayList<T> {
+public class MyArrayList<T> implements MyList<T>  {
     private T[] arr;
     private int size;
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
 }
